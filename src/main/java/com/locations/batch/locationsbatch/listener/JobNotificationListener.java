4af41639b3474b5ea3bce33ff3e3becd2c1408a9
Wellington Gonçalves Pires <wellington.pires@mercadolivre.com>
2019-11-22 15:21:12 -0300

    First commit

---
+++ src/main/java/com/locations/batch/locationsbatch/listener/JobNotificationListener.java
@@ -0,0 +1,36 @@
+package com.locations.batch.locationsbatch.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.locations.batch.locationsbatch.repository.LocationRepository;
+
+@Component
+public class JobNotificationListener extends JobExecutionListenerSupport {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Autowired
+	private LocationRepository locationRepository;
+
+	@Override
+	public void beforeJob(JobExecution jobExecution) {
+		logger.info("O Job {} foi iniciado ás {}", jobExecution.getJobConfigurationName());
+	}
+
+	@Override
+	public void afterJob(JobExecution jobExecution) {
+
+		if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+			logger.info("O Job {} finalizou com sucesso!", jobExecution.getJobConfigurationName());
+			locationRepository.findAll().stream().forEach(System.out::println);
+		}
+
+	}
+
+}
