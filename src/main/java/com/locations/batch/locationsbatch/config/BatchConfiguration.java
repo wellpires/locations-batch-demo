b87c71f35aef71c60bd7d61517b00efe49871ed5
Wellington Gonçalves Pires <wellington.pires@mercadolivre.com>
2019-11-27 01:32:14 -0300

    Final version to presentation

--- src/main/java/com/locations/batch/locationsbatch/config/BatchConfiguration.java
+++ src/main/java/com/locations/batch/locationsbatch/config/BatchConfiguration.java
@@ -1,16 +1,11 @@
 package com.locations.batch.locationsbatch.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.data.RepositoryItemReader;
-import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
@@ -19,15 +14,12 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.client.RestTemplate;
 
 import com.locations.batch.locationsbatch.dto.LocationDTO;
 import com.locations.batch.locationsbatch.listener.JobNotificationListener;
 import com.locations.batch.locationsbatch.model.Location;
 import com.locations.batch.locationsbatch.processor.LocationItemProcessor;
-import com.locations.batch.locationsbatch.processor.UncompletedItemProcessor;
-import com.locations.batch.locationsbatch.repository.LocationRepository;
 import com.locations.batch.locationsbatch.writer.LocationItemWriter;
 
 @Configuration
@@ -39,9 +31,6 @@ public class BatchConfiguration {
 
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
-
-	@Autowired
-	private LocationRepository locationRepository;
 
 	@Bean
 	public RestTemplate restTemplate() {
@@ -64,32 +53,15 @@ public class BatchConfiguration {
 	}
 
 	@Bean
-	public RepositoryItemReader<Location> repositoryItemReader() {
-		Map<String, Direction> sort = new HashMap<>();
-		sort.put("id", Direction.ASC);
-
-		return new RepositoryItemReaderBuilder<Location>().name("uncompletedLocationsItemReader")
-				.repository(locationRepository).methodName("findByCityIsNullOrDistrictIsNullOrStreetIsNull").sorts(sort)
-				.build();
-	}
-
-	@Bean
 	public Job importLocationJob(JobNotificationListener jobListener, Step step1, Step step2) {
 		return jobBuilderFactory.get("importLocationJob").incrementer(new RunIdIncrementer()).listener(jobListener)
-				.flow(step1).next(step2).end().build();
+				.flow(step1).end().build();
 	}
 
 	@Bean
 	public Step step1(FlatFileItemReader<LocationDTO> reader, LocationItemProcessor processor,
 			LocationItemWriter writer) {
 		return stepBuilderFactory.get("step1").<LocationDTO, Location>chunk(10000).reader(reader).processor(processor)
-				.writer(writer).build();
-	}
-
-	@Bean
-	public Step step2(RepositoryItemReader<Location> reader, UncompletedItemProcessor processor,
-			LocationItemWriter writer) {
-		return stepBuilderFactory.get("step2").<Location, Location>chunk(100).reader(reader).processor(processor)
 				.writer(writer).build();
 	}
 
